95984684f04f5a6368616cfc7d79a412759b8044
iahmedelbayaa <elbayaaahmed845@gmail.com>
2022-04-07 23:51:43 +0200

    get-data-from-backend

--- README.md
+++ README.md
@@ -13,4 +13,16 @@
    1. npm i react-router-dom
    2. create router for home screen
    3. create router for product screen
-5. 
+5. Create Node.Js Server
+   1. run npm init in root folder
+   2. update package.json set type: module
+   3. add .js to imports
+   4. npm install express
+   5. create server.js
+   6. add start command as node backend/server.js
+   7. require express
+   8. create route for /return backend is ready
+   9. move products.js from frontend to backend
+   10. create route from api/products
+   11. return products
+   12. run npm start
---
+++ backend/data.js
@@ -0,0 +1,55 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirts',
+      slug: 'nike-slim-shirts',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'Adidas-Fit-Shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality shirt',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-Pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'High quality shirt',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'Adidas-Fit-Pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality Pant',
+    },
+  ],
+};
+
+
+export default data;
